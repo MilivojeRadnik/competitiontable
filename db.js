818807a1d2dbcf4598e1fa7cfedef79867c6a89b
Milivoje <djox2003@gmail.com>
2023-09-22 15:19:23 +0200

    fixed the most stupid mistake ever

--- db.js
+++ db.js
@@ -1,12 +1,10 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host:
-    process.env.HOST ||
-    'competitiondb.cpdh7ubbl10e.eu-north-1.rds.amazonaws.com',
-  user: process.env.USER || 'root',
-  password: process.env.PASSWORD || 'Root123*',
-  database: process.env.DATABASE || 'mas_v2',
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABAS,
   waitForConnections: true,
   connectionLimit: 5,
   queueLimit: 0,
